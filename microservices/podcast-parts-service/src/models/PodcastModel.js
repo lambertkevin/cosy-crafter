fb258ed49da20223778df33d6814980a0e2b962e
lambertkevin <kvin.lambert@gmail.com>
2020-08-02 00:11:59 +0200

    Add middlewares to cascade

--- microservices/podcast-parts-service/src/models/PodcastModel.js
+++ microservices/podcast-parts-service/src/models/PodcastModel.js
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+import Boom from '@hapi/boom';
 import mongoose from 'mongoose';
 import mongooseUniqueValidator from 'mongoose-unique-validator';
 import arrayToProjection from '../utils/arrayToProjection';
@@ -38,5 +40,21 @@ const schema = new mongoose.Schema({
 
 // This allow for beautified E11000 errors for 'uniqueness' of fields
 schema.plugin(mongooseUniqueValidator);
+
+// Cascade Delete
+schema.pre('deleteMany', async function preDeleteManyMiddelware(next) {
+  try {
+    const Part = _.get(this, ['mongooseCollection', 'conn', 'models', 'Part']);
+    const ids = await _.get(this, ['_conditions', '_id', '$in'], []);
+    const podcasts = await Promise.all(ids.map((id) => this.model.findById(id)));
+    const partsIds = podcasts.reduce((curr, acc) => [...curr, ..._.get(acc, ['parts'], [])], []);
+
+    await Part.deleteMany({ _id: { $in: partsIds } }).exec();
+
+    next();
+  } catch (e) {
+    next(Boom.boomify(e));
+  }
+});
 
 export default mongoose.model('Podcast', schema);
