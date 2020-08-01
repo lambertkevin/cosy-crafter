31e27b1521760d39cbaa657b035f7133c1fcd160
KevinMolotov <klambert@molotov.tv>
2020-08-01 15:09:23 +0200

    Basic hapi configuration

---
+++ microservices/podcast-parts-service/src/config.js
@@ -0,0 +1,20 @@
+/**
+ * Config for Node
+ * @type {Object}
+ */
+export const nodeConfig = {
+  port: 3000,
+  host: 'localhost'
+};
+
+/**
+ * Config for DB
+ * @type {Object}
+ */
+export const databaseConfig = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  db: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD
+};
