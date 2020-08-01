1395c34bf77026d9bc02fb853d87e86e99191628
lambertkevin <kvin.lambert@gmail.com>
2020-08-01 15:10:14 +0200

    Add podcast api

---
+++ microservices/podcast-parts-service/src/api/PodcastsApi.js
@@ -0,0 +1,46 @@
+import joi from '@hapi/joi';
+import * as podcastController from '../controllers/PodcastController';
+
+export default {
+  name: 'podcastsApi',
+  async register(server) {
+    server.route({
+      method: 'GET',
+      path: '/',
+      handler: podcastController.find
+    });
+
+    server.route({
+      method: 'GET',
+      path: '/{id}',
+      handler: (request) => podcastController.findOne(request.params.id)
+    });
+
+    server.route({
+      method: 'POST',
+      path: '/',
+      handler: (request) => podcastController.create(request.payload),
+      options: {
+        validate: {
+          payload: joi.object({
+            name: joi.string().min(1).max(50).required(),
+            edition: joi.number().positive().required(),
+            tags: joi.array().items(joi.string().min(1).max(50))
+          })
+        }
+      }
+    });
+
+    server.route({
+      method: 'PATCH',
+      path: '/{id}',
+      handler: (request) => podcastController.update(request.params.id, request.payload)
+    });
+
+    server.route({
+      method: 'DELETE',
+      path: '/{id}',
+      handler: (request) => podcastController.remove(request.params.id)
+    });
+  }
+};
