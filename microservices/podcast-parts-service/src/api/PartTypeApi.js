86124f895bc9d2c05f4ea8975444ca24900fda2e
KevinMolotov <klambert@molotov.tv>
2020-08-01 16:57:13 +0200

    Add PartType CRUD API

---
+++ microservices/podcast-parts-service/src/api/PartTypeApi.js
@@ -0,0 +1,44 @@
+import joi from '@hapi/joi';
+import * as partTypeController from '../controllers/PartTypeController';
+
+export default {
+  name: 'partTypeApi',
+  async register(server) {
+    server.route({
+      method: 'GET',
+      path: '/',
+      handler: partTypeController.find
+    });
+
+    server.route({
+      method: 'GET',
+      path: '/{id}',
+      handler: (request) => partTypeController.findOne(request.params.id)
+    });
+
+    server.route({
+      method: 'POST',
+      path: '/',
+      handler: (request) => partTypeController.create(request.payload),
+      options: {
+        validate: {
+          payload: joi.object({
+            name: joi.string().min(1).max(50).required()
+          })
+        }
+      }
+    });
+
+    server.route({
+      method: 'PATCH',
+      path: '/{id}',
+      handler: (request) => partTypeController.update(request.params.id, request.payload)
+    });
+
+    server.route({
+      method: 'DELETE',
+      path: '/{id}',
+      handler: (request) => partTypeController.remove(request.params.id)
+    });
+  }
+};
