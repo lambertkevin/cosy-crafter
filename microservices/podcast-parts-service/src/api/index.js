1998904109da8c19fa9534d5774b540f49432754
lambertkevin <kvin.lambert@gmail.com>
2020-08-01 16:57:13 +0200

    Add PartType CRUD API

--- microservices/podcast-parts-service/src/api/index.js
+++ microservices/podcast-parts-service/src/api/index.js
@@ -1,5 +1,7 @@
 import PodcastApi from './PodcastApi';
+import PartTypeApi from './PartTypeApi';
 
 export default [
-  { plugin: PodcastApi, routes: { prefix: '/v1/podcasts' } }
+  { plugin: PodcastApi, routes: { prefix: '/v1/podcasts' } },
+  { plugin: PartTypeApi, routes: { prefix: '/v1/part-types' } }
 ];
