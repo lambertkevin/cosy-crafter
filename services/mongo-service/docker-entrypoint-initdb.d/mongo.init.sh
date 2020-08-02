14240a91ceb5dcbd813cc29e972046528ea600bc
KevinMolotov <klambert@molotov.tv>
2020-08-02 21:48:59 +0200

    Add mongo init for db creation in first instance

---
+++ services/mongo-service/docker-entrypoint-initdb.d/mongo.init.sh
@@ -0,0 +1,14 @@
+echo 'INITIALIZING MONGO DATABASE. PROUT'
+
+mongo -- "$MONGO_INITDB_DATABASE" <<EOF
+    var rootUser = '$MONGO_INITDB_ROOT_USERNAME';
+    var rootPassword = '$MONGO_INITDB_ROOT_PASSWORD';
+    var admin = db.getSiblingDB('admin');
+    admin.auth(rootUser, rootPassword);
+
+    var podcastPartsUser = '$PODCAST_PARTS_DB_USER';
+    var podcastPartsPwd = '$PODCAST_PARTS_DB_PASSWORD';
+    var podcastPartsDb = '$PODCAST_PARTS_DB_NAME'
+    use $PODCAST_PARTS_DB_NAME;
+    db.createUser({user: podcastPartsUser, pwd: podcastPartsPwd, roles:[{role: "readWrite", db: podcastPartsDb}]})
+EOF
