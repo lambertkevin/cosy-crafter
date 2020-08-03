dcf20a62e7e79fac06c99fd7678bb5b7f7821ca0
lambertkevin <kvin.lambert@gmail.com>
2020-08-03 16:24:15 +0200

    Correct wording for scripts

--- services/podcast-parts-service/run.sh
+++ services/podcast-parts-service/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set +e
 
-echo $NODE_ENV
+echo "Environnement: ${NODE_ENV:-unkown}"
 
 if [ "$NODE_ENV" = "development" ]; then
   npm run dev
 elif [ "$NODE_ENV" = "production" ]; then
   npm run start
 else
-  echo 'NODE_ENV incorrect';
+  echo 'NODE_ENV not set.';
 fi
