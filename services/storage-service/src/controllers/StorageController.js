980ac752c8b05d50b73888647c13da3cf6f38cd6
lambertkevin <kvin.lambert@gmail.com>
2020-08-09 03:12:55 +0200

    Add storage controller w/ route for podcast files

--- services/storage-service/src/controllers/StorageController.js
+++ services/storage-service/src/controllers/StorageController.js
@@ -1,5 +1,38 @@
+import Boom from '@hapi/boom';
+import { v4 as uuid }  from 'uuid';
 import StorageFactory from '../utils/storageFactory';
 
 const storages = StorageFactory();
 
-export default {};
+/**
+ * Save a podcast part file
+ *
+ * @param {Object} payload
+ * @param {ReadableStream} payload.file
+ * @param {String} payload.podcastName
+ * @param {String} payload.filename
+ *
+ * @return {Object}
+ */
+export const addPodcastPartFile = async ({
+  file, podcastName, filename: originalFilename
+}) => {
+  const filename = `${uuid()}.${originalFilename}`;
+  const location = `/podcasts/${podcastName}/${filename}`;
+  const storageStrategy = process.env.NODE_ENV === 'production'
+    ? ['scaleway', 'local']
+    : ['local'];
+  const storageType = await storages.setFileFromReadable(storageStrategy, file, location);
+
+  return storageType
+    ? {
+      filename,
+      location,
+      storageType
+    }
+    : Boom.serverUnavailable('All storages options have failed');
+};
+
+export default {
+  addPodcastPartFile
+};
