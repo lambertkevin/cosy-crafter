2eb791e20441c5228a913fb35c8314cff04757e0
KevinMolotov <klambert@molotov.tv>
2020-08-09 03:03:13 +0200

    Add storage factory with readable abstraction

--- services/storage-service/src/utils/storageFactory.js
+++ services/storage-service/src/utils/storageFactory.js
@@ -3,35 +3,68 @@ import { Storage,  StorageType } from '@tweedegolf/storage-abstraction';
 
 export default () => {
   try {
-    const scaleway = new Storage({
-      type: StorageType.S3,
-      bucketName: process.env.SCALEWAY_BUCKET_NAME,
-      region: process.env.SCALEWAY_REGION,
-      endpoint: process.env.SCALEWAY_ENDPOINT,
-      accessKeyId: process.env.SCALEWAY_ACCESS_KEY_ID,
-      secretAccessKey: process.env.SCALEWAY_SECRET_ACCESS_KEY
-    });
+    const storages = {
+      scaleway: new Storage({
+        type: StorageType.S3,
+        bucketName: process.env.SCALEWAY_BUCKET_NAME,
+        region: process.env.SCALEWAY_REGION,
+        endpoint: process.env.SCALEWAY_ENDPOINT,
+        accessKeyId: process.env.SCALEWAY_ACCESS_KEY_ID,
+        secretAccessKey: process.env.SCALEWAY_SECRET_ACCESS_KEY
+      }),
 
-    const aws = new Storage({
-      type: StorageType.S3,
-      bucketName: process.env.AWS_BUCKET_NAME,
-      region: process.env.AWS_REGION,
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-      useDualStack: true,
-      sslEnabled: true
-    });
+      aws: new Storage({
+        type: StorageType.S3,
+        bucketName: process.env.AWS_BUCKET_NAME,
+        region: process.env.AWS_REGION,
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        useDualStack: true,
+        sslEnabled: true
+      }),
 
-    const local = new Storage({
-      type: StorageType.LOCAL,
-      directory: path.resolve('./bucket'),
-      mode: '750'
-    });
+      local: new Storage({
+        type: StorageType.LOCAL,
+        directory: path.resolve('./bucket'),
+        mode: '750'
+      })
+    };
+
+    /**
+     * Will upload a stream to one of the storage available
+     * depending on a priority list given as argument
+     *
+     * @param {Array|String} _storagesPriority
+     * @param {ReadableStream} stream
+     * @param {String} filepath
+     *
+     * @return {String|void} storageType {aws|scaleway|local|null}
+     */
+    const setFileFromReadable = async (_storagesPriority, stream, filepath) => {
+      const storagesPriority = typeof _storagesPriority === 'string' ? [_storagesPriority] : _storagesPriority;
+      let storageName;
+
+      for (let i = 0; i < storagesPriority.length; i += 1) {
+        storageName = storagesPriority[i];
+        const storage = storages[storageName];
+
+        if (storage) {
+          try {
+            // eslint-disable-next-line no-await-in-loop
+            await storage.addFileFromReadable(stream, filepath);
+            break;
+          } catch (e) {
+            console.log(e);
+            storageName = null;
+          }
+        }
+      }
+
+      return storageName;
+    };
 
     return {
-      aws,
-      scaleway,
-      local
+      setFileFromReadable
     };
   } catch (e) {
     return e;
