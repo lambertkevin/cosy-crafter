a9ed98daf01e0951329352254261ea3e2a82d0db
KevinMolotov <klambert@molotov.tv>
2020-08-09 03:13:11 +0200

    Add Storage Api

--- services/storage-service/src/api/StorageApi.js
+++ services/storage-service/src/api/StorageApi.js
@@ -1,4 +1,42 @@
+import joi from 'joi';
+import failValidationHandler from '../utils/failValidationHandler';
+import * as StorageController from '../controllers/StorageController';
+
 export default {
   name: 'storageApi',
-  async register(server) {}
+  async register(server) {
+    /**
+     * Health Check Route
+     */
+    server.route({
+      method: 'GET',
+      path: '/ping',
+      handler: () => 'pong'
+    });
+
+    /**
+     * Podcast Part Upload Route
+     */
+    server.route({
+      method: 'POST',
+      path: '/podcast-part',
+      options: {
+        payload: {
+          output: 'stream',
+          maxBytes: 200 * 1024 * 1024, // 200 Mo limit on upload size
+          multipart: true,
+          parse: true
+        },
+        validate: {
+          failAction: failValidationHandler,
+          payload: joi.object({
+            podcastName: joi.string().min(1).max(50).required(),
+            filename: joi.string().min(1).max(50).required(),
+            file: joi.any().required()
+          })
+        }
+      },
+      handler: ({ payload }) => StorageController.addPodcastPartFile(payload)
+    });
+  }
 };
