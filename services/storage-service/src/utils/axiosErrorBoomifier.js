bd067b47a10d9d21ca43e039f7b1447639019877
lambertkevin <kvin.lambert@gmail.com>
2020-08-21 16:22:20 +0200

    Fix axios error helper for error before  request

--- services/storage-service/src/utils/axiosErrorBoomifier.js
+++ services/storage-service/src/utils/axiosErrorBoomifier.js
@@ -3,8 +3,12 @@ import Boom from '@hapi/boom';
 
 export default (error) => {
   if (error && error.isAxiosError) {
-    const data = _.get(error, ['response', 'data'], {});
-    return new Boom.Boom(data.message, data);
+    const responseData = _.get(error, ['response', 'data']);
+
+    if (responseData) {
+      return new Boom.Boom(responseData.message, responseData);
+    }
+    return Boom.badRequest(error.message);
   }
   return Boom.boomify(error);
 };
