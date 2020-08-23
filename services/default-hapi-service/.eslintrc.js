c7a5a6d72ddef493d0a3002f375f5d646e2f380d
KevinMolotov <klambert@molotov.tv>
2020-08-23 14:46:08 +0200

    Add default hapi service as boilerplate

---
+++ services/default-hapi-service/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  extends: [
+    '@hapi/eslint-config-hapi',
+    'eslint-config-airbnb-base',
+    'prettier'
+  ],
+  parser: 'babel-eslint',
+  env: {
+    browser: false,
+    node: true,
+    es6: true,
+    mocha: true
+  },
+  rules: {
+    'valid-jsdoc': [
+      'error',
+      {
+        requireReturnType: true,
+        requireReturnDescription: false,
+        requireParamDescription: false
+      }
+    ],
+    'comma-dangle': ['error', 'never'],
+    'no-multi-spaces': 0,
+    'no-underscore-dangle': 0,
+    '@hapi/hapi/scope-start': 0
+  }
+};
