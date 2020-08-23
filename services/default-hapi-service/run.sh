b9403778915472eb25dedddfc6f9cbe4336a889c
lambertkevin <kvin.lambert@gmail.com>
2020-08-23 14:46:08 +0200

    Add default hapi service as boilerplate

---
+++ services/default-hapi-service/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set +e
+
+echo "Environnement: ${NODE_ENV:-unkown}"
+
+if [ "$NODE_ENV" = "development" ]; then
+  npm run dev
+elif [ "$NODE_ENV" = "production" ]; then
+  npm run start
+else
+  echo 'NODE_ENV not set.';
+fi
