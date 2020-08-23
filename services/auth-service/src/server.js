f22ecde260940f8345fec7af32760f5b549a4f06
lambertkevin <kvin.lambert@gmail.com>
2020-08-23 18:06:45 +0200

    Add basic auth service

---
+++ services/auth-service/src/server.js
@@ -0,0 +1,31 @@
+import Hapi from '@hapi/hapi';
+import Inert from '@hapi/inert';
+import Vision from '@hapi/vision';
+import HapiSwagger from 'hapi-swagger';
+import { nodeConfig, swaggerConfig } from './config';
+
+const init = async () => {
+  try {
+    const server = Hapi.server(nodeConfig);
+    await server.register([
+      Inert,
+      Vision,
+      {
+        plugin: HapiSwagger,
+        options: swaggerConfig
+      }
+    ]);
+    await server.start();
+
+    console.log('Server running on %s', server.info.uri);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+process.on('unhandledRejection', (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
