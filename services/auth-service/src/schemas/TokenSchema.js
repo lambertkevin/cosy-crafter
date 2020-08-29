4b5adb2283ca8c1ddebcdd1ed4b573126d87c602
KevinMolotov <klambert@molotov.tv>
2020-08-29 23:07:37 +0200

    Add token API + access/refresh factory

---
+++ services/auth-service/src/schemas/TokenSchema.js
@@ -0,0 +1,33 @@
+import joi from 'joi';
+
+export const hiddenProperties = [];
+
+export const creationSchema = joi
+  .object({
+    type: joi
+      .string()
+      .valid('access', 'refresh')
+      .required()
+      .default('refresh')
+      .example('refresh'),
+    jwtid: joi
+      .string()
+      .length(36)
+      .required()
+      .meta({
+        _mongoose: { unique: true }
+      })
+      .example('123e4567-e89b-12d3-a456-426614174000')
+  })
+  .label('Token');
+
+export const validationSchema = creationSchema;
+
+export const responseSchema = validationSchema
+  .append({
+    _id: joi.string().length(24).required().example('5f3ec14cb2d104269d3c3282')
+  })
+  .fork(hiddenProperties, (x) => x.forbidden())
+  .label('TokenResponse');
+
+export default validationSchema;
