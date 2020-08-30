d6a2b4a25fbac84c95e960bcc2c00665700352cd
KevinMolotov <klambert@molotov.tv>
2020-08-30 19:30:06 +0200

    Remove useless private makes from rsa utils in podcast service

--- services/podcast-service/src/utils/RsaUtils.js
+++ services/podcast-service/src/utils/RsaUtils.js
@@ -4,40 +4,6 @@ import NodeRsa from 'node-rsa';
 
 /**
  * Factory of function capable of decrypting
- * with the private rsa key
- *
- * @return {Function}
- */
-export const makeRsaPrivateDecrypter = () => {
-  const privateKey = new NodeRsa(
-    fs.readFileSync(
-      path.join(process.env.RSA_KEYS_LOCATION, process.env.RSA_KEYS_NAME)
-    ),
-    'pkcs1-private-pem'
-  );
-
-  return (data, format = 'utf8') => privateKey.decrypt(data, format);
-};
-
-/**
- * Factory of function capable of encrypting
- * with the private rsa key
- *
- * @return {Function}
- */
-export const makeRsaPrivateEncrypter = () => {
-  const privateKey = new NodeRsa(
-    fs.readFileSync(
-      path.join(process.env.RSA_KEYS_LOCATION, process.env.RSA_KEYS_NAME)
-    ),
-    'pkcs1-private-pem'
-  );
-
-  return (data, format = 'base64') => privateKey.encrypt(data, format);
-};
-
-/**
- * Factory of function capable of decrypting
  * with the public rsa key
  *
  * @return {Function}
