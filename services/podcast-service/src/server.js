827869b299ece5af1116a5a9075151e3d0ee685a
lambertkevin <kvin.lambert@gmail.com>
2020-08-30 19:28:11 +0200

    Add auth functions to podcast service

--- services/podcast-service/src/server.js
+++ services/podcast-service/src/server.js
@@ -2,12 +2,15 @@ import Hapi from '@hapi/hapi';
 import Inert from '@hapi/inert';
 import Vision from '@hapi/vision';
 import HapiSwagger from 'hapi-swagger';
+import HapiJWT from 'hapi-auth-jwt2';
 import { nodeConfig, swaggerConfig } from './config';
+import { auth } from './auth';
 import apis from './api';
 import db from './database';
 
 const init = async () => {
   const server = Hapi.server(nodeConfig);
+  await auth();
   await db();
   await server.register([
     Inert,
@@ -15,8 +18,16 @@ const init = async () => {
     {
       plugin: HapiSwagger,
       options: swaggerConfig
-    }
+    },
+    HapiJWT
   ]);
+
+  server.auth.strategy('service-jwt', 'jwt', {
+    key: process.env.SERVICE_JWT_SECRET,
+    validate: async (decoded) =>
+      decoded.service ? { isValid: true } : { isValid: false }
+  });
+
   await server.register(apis);
   await server.start();
   console.log('Server running on %s', server.info.uri);
