9dfd72d47fdb81a748ea6d1c712d187001f66350
KevinMolotov <klambert@molotov.tv>
2020-08-30 20:35:33 +0200

    Add auth to storage-service

--- services/storage-service/src/server.js
+++ services/storage-service/src/server.js
@@ -2,26 +2,32 @@ import Hapi from '@hapi/hapi';
 import Inert from '@hapi/inert';
 import Vision from '@hapi/vision';
 import HapiSwagger from 'hapi-swagger';
+import HapiJWT from 'hapi-auth-jwt2';
 import { nodeConfig, swaggerConfig } from './config';
-import PodcastStorageApi from './api/PodcastStorageApi';
+import apis from './api';
+import { auth } from './auth';
 
 const init = async () => {
   try {
     const server = Hapi.server(nodeConfig);
+    await auth();
     await server.register([
       Inert,
       Vision,
+      HapiJWT,
       {
         plugin: HapiSwagger,
         options: swaggerConfig
       }
     ]);
-    await server.register({
-      plugin: PodcastStorageApi,
-      routes: {
-        prefix: '/v1/podcast-parts'
-      }
+
+    server.auth.strategy('service-jwt', 'jwt', {
+      key: process.env.SERVICE_JWT_SECRET,
+      validate: async (decoded) =>
+        decoded.service ? { isValid: true } : { isValid: false }
     });
+
+    await server.register(apis);
     await server.start();
 
     console.log('Server running on %s', server.info.uri);
