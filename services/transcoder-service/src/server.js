0c07548ccd055b0aac330abb22236bb8c7998700
KevinMolotov <klambert@molotov.tv>
2020-08-31 17:48:26 +0200

    Add basic express/socketio service as transcoder

---
+++ services/transcoder-service/src/server.js
@@ -0,0 +1,15 @@
+import os from 'os';
+import express from 'express';
+import socket from 'socket.io';
+import { nodeConfig } from './config';
+
+const app = express();
+const server = app.listen(nodeConfig.port, () => {
+  console.log(`Server running on http://${os.hostname()}:${nodeConfig.port}`);
+});
+
+const io = socket.listen(server);
+
+io.on('connection', async (client) => {
+  console.log('connected');
+});
