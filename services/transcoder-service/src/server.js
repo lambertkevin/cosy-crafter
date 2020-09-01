efcb78f0bca918e4eefce86d0ebd7a3dc1cab8cb
KevinMolotov <klambert@molotov.tv>
2020-09-01 03:12:32 +0200

    Add Auth to transcoder service

--- services/transcoder-service/src/server.js
+++ services/transcoder-service/src/server.js
@@ -2,15 +2,21 @@ import os from 'os';
 import express from 'express';
 import socket from 'socket.io';
 import { nodeConfig } from './config';
+import { auth } from './auth';
 import apis from './api';
 
-const app = express();
-const server = app.listen(nodeConfig.port, () => {
-  console.log(`Server running on http://${os.hostname()}:${nodeConfig.port}`);
-});
+const init = async () => {
+  await auth();
+  const app = express();
+  const server = app.listen(nodeConfig.port, () => {
+    console.log(`Server running on http://${os.hostname()}:${nodeConfig.port}`);
+  });
 
-const io = socket.listen(server);
+  const io = socket.listen(server);
 
-io.on('connection', async (client) => {
-  apis(client);
-});
+  io.on('connection', async (client) => {
+    apis(client);
+  });
+};
+
+init();
