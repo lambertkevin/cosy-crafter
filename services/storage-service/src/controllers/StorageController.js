e13e41001b736893a5797636e20393e7685d8358
KevinMolotov <klambert@molotov.tv>
2020-09-02 19:09:21 +0200

    Add get and delete craft methods to storage service

--- services/storage-service/src/controllers/StorageController.js
+++ services/storage-service/src/controllers/StorageController.js
@@ -127,7 +127,8 @@ export const removePodcastPartFile = async ({
  * @return {Promise<Object>}
  */
 export const addCraftFile = async ({ file, filename }) => {
-  const location = `crafts/${filename}`;
+  const storagePath = `crafts`;
+  const location = `${storagePath}/${filename}`;
   const storageStrategy = ['local'];
   const storedFile = await storages.setFileFromReadable(
     storageStrategy,
@@ -137,14 +138,97 @@ export const addCraftFile = async ({ file, filename }) => {
 
   return storedFile
     ? calibrate.response({
-        location,
+        storagePath,
         storageType: storedFile.storageName,
+        storageFilename: filename,
         publicLink: storedFile.publicLink
       })
     : Boom.serverUnavailable('All storages options have failed');
 };
 
+/**
+ * Save a craft file
+ *
+ * @param {String} id
+ * @param {Object} h
+ *
+ * @return {Response}
+ */
+export const getCraftFile = async (id, h) => {
+  const axiosAsService = makeAxiosInstance();
+  return axiosAsService
+    .get(
+      `http://${PODCAST_SERVICE_NAME}:${PODCAST_SERVICE_PORT}/v1/crafts/${id}`,
+      {
+        headers: {
+          authorization: tokens.accessToken
+        }
+      }
+    )
+    .then(({ data }) => data)
+    .then(async ({ data }) => {
+      const stream = await storages.getFileAsReadable(
+        data.storageType,
+        data.storagePath,
+        data.storageFilename
+      );
+
+      return h
+        .response(stream)
+        .type('audio/mpeg')
+        .header(
+          'content-disposition',
+          `attachment; filename=${_.snakeCase(data.name)}.mp3;`
+        );
+    })
+    .catch((error) => {
+      if (error.isAxiosError) {
+        return axiosErrorBoomifier(error);
+      }
+      if (error.code === 'ENOENT' || error.statusCode === 404) {
+        return Boom.resourceGone('File has been deleted in storage');
+      }
+      if (error.isBoom) {
+        return error;
+      }
+      if (error.statusCode) {
+        return new Boom.Boom(error.message, { statusCode: error.statusCode });
+      }
+      return Boom.boomify(error);
+    });
+};
+
+/**
+ * Remove a craft file
+ *
+ * @param {Object} data
+ * @param {String} data.storageType
+ * @param {String} data.storagePath
+ * @param {String} data.storageFilename
+ *
+ * @return {Object}
+ */
+export const removeCraftFile = async ({
+  storageType,
+  storagePath,
+  storageFilename
+}) => {
+  try {
+    await storages.removeFile(storageType, storagePath, storageFilename);
+    return calibrate.response({ deleted: storageFilename });
+  } catch (e) {
+    if (e.isBoom) {
+      return e;
+    }
+    return Boom.badData();
+  }
+};
+
 export default {
   addPodcastPartFile,
-  getPodcastPartFile
+  getPodcastPartFile,
+  removePodcastPartFile,
+  addCraftFile,
+  getCraftFile,
+  removeCraftFile
 };
