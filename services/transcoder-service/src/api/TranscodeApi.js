4cbc0156a4890f1725ad82d3ba8cff335e0a0f2a
lambertkevin <kvin.lambert@gmail.com>
2020-09-06 00:50:34 +0200

    add validation to transcode api

--- services/transcoder-service/src/api/TranscodeApi.js
+++ services/transcoder-service/src/api/TranscodeApi.js
@@ -1,19 +1,46 @@
+import joi from 'joi';
 import { forEach } from 'lodash';
 import { createTranscodeJob } from '../controllers/TransodeController';
 
 const routes = [
   {
     path: '/join',
-    handler: createTranscodeJob
+    handler: createTranscodeJob,
+    validation: joi.object({
+      files: joi.array().items(
+        joi.object({
+          id: joi.string().length(24).required(),
+          type: joi.string().valid('podcast-part', 'user-input').required(),
+          seek: joi
+            .object({
+              start: joi.number().optional(),
+              end: joi.number().optional()
+            })
+            .optional()
+        })
+      ),
+      name: joi.string().required()
+    })
   }
 ];
 
 export default (prefix, socket) => {
   forEach(routes, (route) => {
     const path = `${prefix}${route.path}`;
-    const hander = (data, ack) =>
-      route.handler.apply(null, [data, ack, socket]);
+    const handler = async (data, ack) => {
+      try {
+        if (route.validation) {
+          await route.validation.validateAsync(data);
+        }
+        return route.handler.apply(null, [data, ack, socket]);
+      } catch (e) {
+        return ack({
+          statusCode: 409,
+          message: 'Bad Request'
+        });
+      }
+    };
 
-    socket.on(path, hander);
+    socket.on(path, handler);
   });
 };
