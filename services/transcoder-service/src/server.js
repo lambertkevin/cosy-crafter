54e2e321e6d4f44e90c1de8f2454ce83aed039a7
KevinMolotov <klambert@molotov.tv>
2020-09-07 13:13:46 +0200

    Add logger for transcoder service init server

--- services/transcoder-service/src/server.js
+++ services/transcoder-service/src/server.js
@@ -1,22 +1,35 @@
 import os from 'os';
 import express from 'express';
 import socket from 'socket.io';
+import { logger } from './utils/Logger';
 import { nodeConfig } from './config';
 import { auth } from './auth';
 import apis from './api';
 
 const init = async () => {
-  await auth();
-  const app = express();
-  const server = app.listen(nodeConfig.port, () => {
-    console.log(`Server running on http://${os.hostname()}:${nodeConfig.port}`);
-  });
+  try {
+    await auth();
+    const app = express();
+    const server = app.listen(nodeConfig.port, () => {
+      console.log(
+        `Server running on http://${os.hostname()}:${nodeConfig.port}`
+      );
+    });
 
-  const io = socket.listen(server);
+    const io = socket.listen(server);
 
-  io.on('connection', async (client) => {
-    apis(client);
-  });
+    io.on('connection', async (client) => {
+      apis(client);
+    });
+  } catch (err) {
+    logger.fatal('Fatal Error while starting the service', err);
+    process.exit(0);
+  }
 };
+
+process.on('unhandledRejection', (err) => {
+  logger.error('unhandledRejection', err);
+  process.exit(1);
+});
 
 init();
