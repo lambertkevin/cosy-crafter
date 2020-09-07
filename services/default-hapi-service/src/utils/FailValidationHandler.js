ee326359949d69fc3de568671a1019b2ba86f09c
lambertkevin <kvin.lambert@gmail.com>
2020-09-07 22:30:08 +0200

    Make FailValidationHandler safe for logging

--- services/default-hapi-service/src/utils/FailValidationHandler.js
+++ services/default-hapi-service/src/utils/FailValidationHandler.js
@@ -1,21 +1,33 @@
+import stream from 'stream';
 import Boom from '@hapi/boom';
-
 /**
  * Async function that allows errors to be shown as
  * API responses when in dev mode
  * @param {Object} request
  * @param {Object} h
- * @param {Object} err
+ * @param {Object} error
  * @return {Promise}
  */
-export default async (request, h, err) => {
+export default async (request, h, error) => {
+  // Some destruc to avoid trying to log Buffer from file.
+  // If logged, it stuck the Node main process and makes the service unavailable
+  const safeOriginal = Object.keys(error._original).reduce((acc, key) => {
+    if (Buffer.isBuffer(error._original[key])) {
+      acc[key] = 'Buffer<Filtered>';
+    } else if (error._original[key] instanceof stream.Readable) {
+      acc[key] = 'Readable<Filtered>';
+    } else {
+      acc[key] = error._original[key];
+    }
+    return acc;
+  }, {});
+  const safeError = { ...error, _original: safeOriginal };
+  console.log(safeError);
   if (process.env.NODE_ENV === 'prod') {
     // In prod, log a limited error message and throw the default Bad Request error.
-    console.error('ValidationError:', err.message); // Better to use an actual logger here.
     throw Boom.badRequest('Invalid request payload input');
   } else {
     // During development, log and respond with the full error.
-    console.error(err);
-    throw err;
+    throw error;
   }
 };
--- services/podcast-service/src/utils/FailValidationHandler.js
+++ services/podcast-service/src/utils/FailValidationHandler.js
@@ -1,3 +1,4 @@
+import stream from 'stream';
 import Boom from '@hapi/boom';
 import { logger } from './Logger';
 
@@ -10,7 +11,20 @@ import { logger } from './Logger';
  * @return {Promise}
  */
 export default async (request, h, error) => {
-  logger.warn('ValidationError', error);
+  // Some destruc to avoid trying to log Buffer from file.
+  // If logged, it stuck the Node main process and makes the service unavailable
+  const safeOriginal = Object.keys(error._original).reduce((acc, key) => {
+    if (Buffer.isBuffer(error._original[key])) {
+      acc[key] = 'Buffer<Filtered>';
+    } else if (error._original[key] instanceof stream.Readable) {
+      acc[key] = 'Readable<Filtered>';
+    } else {
+      acc[key] = error._original[key];
+    }
+    return acc;
+  }, {});
+  const safeError = { ...error, _original: safeOriginal };
+  logger.warn('ValidationError', safeError);
   if (process.env.NODE_ENV === 'prod') {
     // In prod, log a limited error message and throw the default Bad Request error.
     throw Boom.badRequest('Invalid request payload input');
