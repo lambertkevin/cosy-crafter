3fc98ceda37ab8b61416a779ecdc0338c199472b
lambertkevin <kvin.lambert@gmail.com>
2020-09-17 20:51:59 +0200

    Add retry functionality to queue + linting and docs

--- services/pool-service/src/factories/QueueFactory.js
+++ services/pool-service/src/factories/QueueFactory.js
@@ -20,7 +20,6 @@ const JOB_LIST_SAVE_FILE = path.join(path.resolve('./'), '.job-save.json');
 const prioritizeArray = (arr) => _.orderBy(arr, ['priority'], ['desc']);
 
 export const makeQueue = () => {
-  const freezeProps = ['events'];
   const workers = [];
   let jobs = (() => {
     try {
@@ -34,7 +33,7 @@ export const makeQueue = () => {
         ? prioritizeArray(savedJobs.map((x) => makeJob(null, x)))
         : [];
     } catch (error) {
-      logger.error('Error while getting job list save', error);
+      logger.debug('Error while getting job list save', error);
       return [];
     }
   })();
@@ -68,65 +67,99 @@ export const makeQueue = () => {
       return jobs.filter(({ status }) => status === JOB_STATUS_ONGOING);
     },
 
+    /**
+     * Add a job to the queue
+     *
+     * @param {Object} job
+     *
+     * @return {void}
+     */
     addJob(job) {
       jobs = prioritizeArray([...jobs, job]);
+      logger.info('Job added to queue', { job });
       this.events.emit('jobs-updated', { jobs: jobs.length });
     },
 
+    /**
+     * Remove a job from the queue
+     *
+     * @param {Object} job
+     *
+     * @return {void}
+     */
     removeJob(job) {
       _.pull(jobs, job);
+      logger.info('Job removed from queue', { job });
       this.events.emit('jobs-updated', { jobs: jobs.length });
     },
 
+    /**
+     * Add a worker to the queue
+     *
+     * @param {Object} worker
+     *
+     * @return {void}
+     */
     addWorker(worker) {
       workers.push(worker);
+      logger.info('Worker added to queue', { worker });
       this.events.emit('workers-updated', { workers });
     },
 
+    /**
+     * Remove a worker from the queue
+     *
+     * @param {Object} worker
+     *
+     * @return {void}
+     */
     removeWorker(worker) {
-      _.pull(this.failedJobsworkers, worker);
+      _.pull(workers, worker);
+      logger.info('Worker removed from queue', { worker });
       this.events.emit('workers-updated', { workers });
     },
 
+    /**
+     * Launch the next job
+     *
+     * @return {void}
+     */
     async next() {
       if (this.availableWorkers.length && this.waitingJobs.length) {
         const availableWorker = this.availableWorkers[0];
         const job = this.waitingJobs[0];
-        job.events.on('job-status-changed', () => {
+        const onJobStatusChanged = () => {
           this.events.emit('jobs-updated');
-        });
+        };
+        job.events.on('job-status-changed', onJobStatusChanged);
 
         try {
           await availableWorker.execute(job);
-          console.log('Job finished in queue');
-        } catch (e) {
-          console.log('Job failed in queue');
+          job.finish();
+          this.removeJob(job);
+        } catch {
+          const error = job.retry();
+
+          if (error.name !== 'RetryError') {
+            this.removeJob(job);
+          }
         }
 
-        process.nextTick(_.throttle(this.next.bind(this), 1000));
+        job.events.off('job-status-changed', onJobStatusChanged);
+        process.nextTick(
+          _.throttle(
+            this.next.bind(this),
+            process.env.NODE_ENV === 'development' ? 2000 : 500
+          )
+        );
       }
     },
 
-    onJobsUpdated() {
-      fs.writeFileSync(
-        JOB_LIST_SAVE_FILE,
-        stringify({ jobs }, (key, val) => {
-          return typeof val === 'function' ? val.toString() : val;
-        })
-      );
-
-      if (this.availableWorkers.length && this.waitingJobs.length) {
-        this.next();
-      }
-    },
-
-    onWorkersUpdated() {
-      console.log('workers: ', workers.length);
-      if (this.availableWorkers.length && this.waitingJobs.length) {
-        this.next();
-      }
-    },
-
+    /**
+     * Start listening the events linked to jobs & workers updates
+     *
+     * @return {void}
+     */
     registerEvents() {
       this.events.on('jobs-updated', () => {
         console.log('Job List Updated', jobs.length);
@@ -151,6 +184,7 @@ export const makeQueue = () => {
     }
   };
 
+  const freezeProps = ['events'];
   freezeProps.forEach((freezeProp) => {
     Object.defineProperty(queue, freezeProp, {
       writable: false,
