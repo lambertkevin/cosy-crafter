b323a309bef29c0ff9aa0876d352509b531c048e
lambertkevin <kvin.lambert@gmail.com>
2020-09-19 14:00:25 +0200

    Add pool keys creation on pre-build script

--- pre-build.sh
+++ pre-build.sh
@@ -4,3 +4,5 @@ set +e
 mkdir ./.keys
 ssh-keygen -t rsa -b 4096 -C 'auth-service' -f ./.keys/auth -N '' -m pem
 ssh-keygen -f ./.keys/auth.pub -e -m pem > ./.keys/auth.pem
+ssh-keygen -t rsa -b 4096 -C 'pool-service' -f ./.keys/pool -N '' -m pem
+ssh-keygen -f ./.keys/pool.pub -e -m pem > ./.keys/pool.pem
