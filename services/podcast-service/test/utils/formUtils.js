04559b3efc522210aaf61f1470f73eeb7f173a79
KevinMolotov <klambert@molotov.tv>
2021-02-20 01:30:51 +0100

    Move test utils to their own files

---
+++ services/podcast-service/test/utils/formUtils.js
@@ -0,0 +1,20 @@
+import FormData from 'form-data';
+
+/**
+ * Creates a Formdata from an object by appending each property
+ *
+ * @param {Object} obj
+ *
+ * @return {FormData}
+ */
+export const objectToFormData = (obj) => {
+  const fd = new FormData();
+  Object.keys(obj).forEach((key) => {
+    fd.append(key, obj[key]);
+  });
+  return fd;
+};
+
+export default {
+  objectToFormData
+};
