20c04f4919023924b863247f65a6a5c854d2230d
lambertkevin <kvin.lambert@gmail.com>
2021-02-20 13:01:19 +0100

    Add required fields test to parts

--- services/podcast-service/test/e2e/parts.test.js
+++ services/podcast-service/test/e2e/parts.test.js
@@ -38,16 +38,25 @@ describe('Parts API tests', () => {
     });
   });
 
-  describe('Parts Creation', () => {
+  describe('Parts Creation and Deletion', () => {
     let fakePartPayloadFormData;
     let fakePartPayloadString;
     let partPayloadFormData;
     let partPayloadString;
+    let fakePartPayload;
     let section;
     let podcast;
     let part;
 
     before(async () => {
+      fakePartPayload = {
+        name: `part`,
+        section: '1234-1234-1234-1234-1234',
+        podcast: '1234-1234-1234-1234-1234',
+        tags: 'tag1',
+        file: Buffer.alloc(0)
+      };
+
       section = await SectionController.create({
         name: `e2e-test`
       });
@@ -55,14 +64,6 @@ describe('Parts API tests', () => {
         name: `e2e-podcast`,
         edition: 1
       });
-
-      const fakePartPayload = {
-        name: `part`,
-        section: '1234-1234-1234-1234-1234',
-        podcast: '1234-1234-1234-1234-1234',
-        tags: 'tag1',
-        file: Buffer.alloc(0)
-      };
 
       fakePartPayloadFormData = objectToFormData(fakePartPayload);
       fakePartPayloadString = await getStream(fakePartPayloadFormData);
@@ -79,163 +80,289 @@ describe('Parts API tests', () => {
       partPayloadString = await getStream(partPayloadFormData);
     });
 
-    after(async () => {
-      await PartController.remove([part?.data?._id]);
-      await SectionController.remove([section?.data?._id]);
-      await PodcastController.remove([podcast?.data?._id]);
-    });
-
-    it('should fail trying to create part without jwt', () => {
-      return server
-        .inject({
-          method: 'POST',
-          url: '/v1/parts',
-          payload: fakePartPayloadString,
-          headers: {
-            ...fakePartPayloadFormData.getHeaders(),
-            maxBodyLength: 200 * 1024 * 1024, // 200MB max part size
-            maxContentLength: 200 * 1024 * 1024 // 200MB max part size
-          }
-        })
-        .then((response) => {
-          expect(response).to.be.a('object');
-          expect(response).to.include({ statusCode: 401 });
-          expect(response.result).to.deep.include({
-            statusCode: 401,
-            error: 'Unauthorized',
-            message: 'Missing authentication'
+    describe('Fails', () => {
+      it('should fail trying to create part without jwt. HTTP 401', () => {
+        return server
+          .inject({
+            method: 'POST',
+            url: '/v1/parts',
+            payload: fakePartPayloadString,
+            headers: {
+              ...fakePartPayloadFormData.getHeaders(),
+              maxBodyLength: 200 * 1024 * 1024, // 200MB max part size
+              maxContentLength: 200 * 1024 * 1024 // 200MB max part size
+            }
+          })
+          .then((response) => {
+            expect(response).to.be.a('object');
+            expect(response).to.include({ statusCode: 401 });
+            expect(response.result).to.deep.include({
+              statusCode: 401,
+              error: 'Unauthorized',
+              message: 'Missing authentication'
+            });
           });
-        });
-    });
-
-    it('should fail if creation content-type is not multipart', () => {
-      return server
-        .inject({
-          method: 'POST',
-          url: '/v1/parts',
-          payload: fakePartPayloadString,
-          headers: {
-            // No content-type is set so 'application/json' is set as default
-            authorization: accessToken
-          }
-        })
-        .then((response) => {
-          expect(response).to.be.a('object');
-          expect(response).to.include({ statusCode: 415 });
-          expect(response.result).to.deep.include({
-            statusCode: 415,
-            error: 'Unsupported Media Type',
-            message: 'Unsupported Media Type'
-          });
-        });
-    });
-
-    it("should fail if part dependencies doesn't exist", () => {
-      return server
-        .inject({
-          method: 'POST',
-          url: '/v1/parts',
-          payload: fakePartPayloadString,
-          headers: {
-            ...fakePartPayloadFormData.getHeaders(),
-            authorization: accessToken
-          }
-        })
-        .then((response) => {
-          expect(response).to.be.a('object');
-          expect(response).to.include({ statusCode: 406 });
-          expect(response.result).to.deep.include({
-            statusCode: 406,
-            error: 'Not Acceptable',
-            message: "At least one dependency doesn't exist"
-          });
-        });
-    });
-
-    it('should succeed creating part', () => {
-      return server
-        .inject({
-          method: 'POST',
-          url: '/v1/parts',
-          payload: partPayloadString,
-          headers: {
-            ...partPayloadFormData.getHeaders(),
-            authorization: accessToken
-          }
-        })
-        .then((response) => {
-          part = response.result;
-          expect(response).to.be.a('object');
-          expect(response).to.include({ statusCode: 200 });
-          expect(response?.result).to.include({
-            statusCode: 200
-          });
-          expect(response?.result?.data).to.include({
-            originalFilename: 'blank.mp3',
-            contentType: 'audio/mpeg'
-          });
-          expect(response?.result?.data?.section?.toString()).to.be.equal(
-            section?.data?._id?.toString()
-          );
-          expect(response?.result?.data?.podcast?.toString()).to.be.equal(
-            podcast?.data?._id?.toString()
-          );
-        });
-    });
-
-    it('should succeed deleting part', () => {
-      return server
-        .inject({
-          method: 'DELETE',
-          url: '/v1/parts',
-          payload: { ids: [part?.data?._id?.toString()] },
-          headers: {
-            authorization: accessToken
-          }
-        })
-        .then((response) => {
-          expect(response).to.be.a('object');
-          expect(response).to.include({ statusCode: 200 });
-          expect(response?.result).to.include({
-            statusCode: 200
-          });
-          expect(response?.result?.data?.deleted[0]).to.be.equal(
-            part?.data?._id?.toString()
-          );
-        });
-    });
-
-    it('should cascade delete part when deleting podcast', async () => {
-      const part2 = await PartController.create({
-        name: `part2`,
-        section: section?.data?._id?.toString(),
-        podcast: podcast?.data?._id?.toString(),
-        tags: 'tag1',
-        file: {
-          path: path.join(path.resolve('./'), 'test', 'files', 'blank.mp3'),
-          bytes: 61637,
-          filename: 'blank.mp3',
-          headers: {
-            'content-disposition':
-              'form-data; name="file"; filename="blank.mp3"',
-            'content-type': 'audio/mpeg'
-          }
-        }
       });
-      await PodcastController.remove([podcast.data._id]);
 
-      return server
-        .inject({
-          method: 'GET',
-          url: `/v1/parts/${part2.data._id.toString()}`,
-          headers: {
-            authorization: accessToken
-          }
-        })
-        .then((response) => {
-          expect(response).to.be.a('object');
-          expect(response).to.include({ statusCode: 404 });
+      it('should fail if creation content-type is not multipart. HTTP 415', () => {
+        return server
+          .inject({
+            method: 'POST',
+            url: '/v1/parts',
+            payload: fakePartPayloadString,
+            headers: {
+              contentType: 'application/json',
+              authorization: accessToken
+            }
+          })
+          .then((response) => {
+            expect(response).to.be.a('object');
+            expect(response).to.include({ statusCode: 415 });
+            expect(response.result).to.deep.include({
+              statusCode: 415,
+              error: 'Unsupported Media Type',
+              message: 'Unsupported Media Type'
+            });
+          });
+      });
+
+      it("should fail if part dependencies doesn't exist. HTTP 406", () => {
+        return server
+          .inject({
+            method: 'POST',
+            url: '/v1/parts',
+            payload: fakePartPayloadString,
+            headers: {
+              ...fakePartPayloadFormData.getHeaders(),
+              authorization: accessToken
+            }
+          })
+          .then((response) => {
+            expect(response).to.be.a('object');
+            expect(response).to.include({ statusCode: 406 });
+            expect(response.result).to.deep.include({
+              statusCode: 406,
+              error: 'Not Acceptable',
+              message: "At least one dependency doesn't exist"
+            });
+          });
+      });
+
+      describe('Required fields', () => {
+        it('should fail if missing name', async () => {
+          const payloadFormData = objectToFormData({
+            ...fakePartPayload,
+            name: null
+          });
+          const payloadString = await getStream(payloadFormData);
+
+          return server
+            .inject({
+              method: 'POST',
+              url: '/v1/parts',
+              payload: payloadString,
+              headers: {
+                ...payloadFormData.getHeaders(),
+                maxBodyLength: 200 * 1024 * 1024, // 200MB max part size
+                maxContentLength: 200 * 1024 * 1024, // 200MB max part size
+                authorization: accessToken
+              }
+            })
+            .then((response) => {
+              expect(response).to.be.a('object');
+              expect(response).to.include({ statusCode: 400 });
+              expect(response.result).to.deep.include({
+                statusCode: 400,
+                error: 'Bad Request',
+                message: '"name" is required'
+              });
+            });
         });
+
+        it('should fail if missing section', async () => {
+          const payloadFormData = objectToFormData({
+            ...fakePartPayload,
+            section: null
+          });
+          const payloadString = await getStream(payloadFormData);
+
+          return server
+            .inject({
+              method: 'POST',
+              url: '/v1/parts',
+              payload: payloadString,
+              headers: {
+                ...payloadFormData.getHeaders(),
+                maxBodyLength: 200 * 1024 * 1024, // 200MB max part size
+                maxContentLength: 200 * 1024 * 1024, // 200MB max part size
+                authorization: accessToken
+              }
+            })
+            .then((response) => {
+              expect(response).to.be.a('object');
+              expect(response).to.include({ statusCode: 400 });
+              expect(response.result).to.deep.include({
+                statusCode: 400,
+                error: 'Bad Request',
+                message: '"section" is required'
+              });
+            });
+        });
+
+        it('should fail if missing podcast', async () => {
+          const payloadFormData = objectToFormData({
+            ...fakePartPayload,
+            podcast: null
+          });
+          const payloadString = await getStream(payloadFormData);
+
+          return server
+            .inject({
+              method: 'POST',
+              url: '/v1/parts',
+              payload: payloadString,
+              headers: {
+                ...payloadFormData.getHeaders(),
+                maxBodyLength: 200 * 1024 * 1024, // 200MB max part size
+                maxContentLength: 200 * 1024 * 1024, // 200MB max part size
+                authorization: accessToken
+              }
+            })
+            .then((response) => {
+              expect(response).to.be.a('object');
+              expect(response).to.include({ statusCode: 400 });
+              expect(response.result).to.deep.include({
+                statusCode: 400,
+                error: 'Bad Request',
+                message: '"podcast" is required'
+              });
+            });
+        });
+
+        it('tag is not required');
+
+        it('should fail if missing file', async () => {
+          const payloadFormData = objectToFormData({
+            ...fakePartPayload,
+            file: null
+          });
+          const payloadString = await getStream(payloadFormData);
+
+          return server
+            .inject({
+              method: 'POST',
+              url: '/v1/parts',
+              payload: payloadString,
+              headers: {
+                ...payloadFormData.getHeaders(),
+                maxBodyLength: 200 * 1024 * 1024, // 200MB max part size
+                maxContentLength: 200 * 1024 * 1024, // 200MB max part size
+                authorization: accessToken
+              }
+            })
+            .then((response) => {
+              expect(response).to.be.a('object');
+              expect(response).to.include({ statusCode: 400 });
+              expect(response.result).to.deep.include({
+                statusCode: 400,
+                error: 'Bad Request',
+                message: '"file" is required'
+              });
+            });
+        });
+      });
     });
+
+    describe('Success', () => {
+      it('should succeed creating part', () => {
+        return server
+          .inject({
+            method: 'POST',
+            url: '/v1/parts',
+            payload: partPayloadString,
+            headers: {
+              ...partPayloadFormData.getHeaders(),
+              authorization: accessToken
+            }
+          })
+          .then((response) => {
+            part = response.result;
+            expect(response).to.be.a('object');
+            expect(response).to.include({ statusCode: 200 });
+            expect(response?.result).to.include({
+              statusCode: 200
+            });
+            expect(response?.result?.data).to.include({
+              originalFilename: 'blank.mp3',
+              contentType: 'audio/mpeg'
+            });
+            expect(response?.result?.data?.section?.toString()).to.be.equal(
+              section?.data?._id?.toString()
+            );
+            expect(response?.result?.data?.podcast?.toString()).to.be.equal(
+              podcast?.data?._id?.toString()
+            );
+          });
+      });
+
+      it('should succeed deleting part', () => {
+        return server
+          .inject({
+            method: 'DELETE',
+            url: '/v1/parts',
+            payload: { ids: [part?.data?._id?.toString()] },
+            headers: {
+              authorization: accessToken
+            }
+          })
+          .then((response) => {
+            expect(response).to.be.a('object');
+            expect(response).to.include({ statusCode: 200 });
+            expect(response?.result).to.include({
+              statusCode: 200
+            });
+            expect(response?.result?.data?.deleted[0]).to.be.equal(
+              part?.data?._id?.toString()
+            );
+          });
+      });
+
+      it('should cascade delete part when deleting podcast', async () => {
+        const part2 = await PartController.create({
+          name: `part2`,
+          section: section?.data?._id?.toString(),
+          podcast: podcast?.data?._id?.toString(),
+          tags: 'tag1',
+          file: {
+            path: path.join(path.resolve('./'), 'test', 'files', 'blank.mp3'),
+            bytes: 61637,
+            filename: 'blank.mp3',
+            headers: {
+              'content-disposition':
+                'form-data; name="file"; filename="blank.mp3"',
+              'content-type': 'audio/mpeg'
+            }
+          }
+        });
+        await PodcastController.remove([podcast.data._id]);
+
+        return server
+          .inject({
+            method: 'GET',
+            url: `/v1/parts/${part2.data._id.toString()}`,
+            headers: {
+              authorization: accessToken
+            }
+          })
+          .then((response) => {
+            expect(response).to.be.a('object');
+            expect(response).to.include({ statusCode: 404 });
+          });
+      });
+    });
+  });
+
+  describe('Parts Update', () => {
+    it('has to be implemented');
   });
 });
--- services/podcast-service/test/utils/formUtils.js
+++ services/podcast-service/test/utils/formUtils.js
@@ -10,7 +10,9 @@ import FormData from 'form-data';
 export const objectToFormData = (obj) => {
   const fd = new FormData();
   Object.keys(obj).forEach((key) => {
-    fd.append(key, obj[key]);
+    if (obj[key]) {
+      fd.append(key, obj[key]);
+    }
   });
   return fd;
 };
