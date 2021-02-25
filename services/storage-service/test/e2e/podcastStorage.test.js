dfd9a652d14abd0ae7aab92cedc6b40c0d2b7a11
KevinMolotov <klambert@molotov.tv>
2021-02-25 17:32:31 +0100

    Add podcastStorage api tests

---
+++ services/storage-service/test/e2e/podcastStorage.test.js
@@ -0,0 +1,341 @@
+import fs from 'fs';
+import path from 'path';
+import { expect } from 'chai';
+import getStream from 'get-stream';
+import { startAuthService, accessToken } from '../utils/authUtils';
+import { objectToFormData } from '../utils/formUtils';
+import mockS3 from '../utils/mockS3';
+import init from '../../src/server';
+
+describe('Podcast Storage API V1 tests', () => {
+  let s3FakeServers;
+  let server;
+  let pid;
+
+  before(async () => {
+    const authServiceChild = await startAuthService();
+    pid = authServiceChild.pid;
+    server = await init();
+    s3FakeServers = await mockS3();
+  });
+
+  after(() => {
+    process.kill(pid);
+    server.stop();
+    fs.rmdirSync(path.resolve('./bucket/tests/'), { recursive: true });
+  });
+
+  describe('Server Testing', () => {
+    it('should validate if parts v1 API is reachable', () => {
+      return server
+        .inject({
+          method: 'GET',
+          url: '/v1/podcast-parts/ping'
+        })
+        .then((response) => {
+          expect(response).to.be.a('object');
+          expect(response).to.include({ statusCode: 200 });
+          expect(response.result).to.be.equal('pong');
+        });
+    });
+  });
+
+  describe('Podcast Part Upload', () => {
+    const podcastPartPayload = {
+      podcastName: 'e2e-podcast',
+      filename: 'e2e-filename.mp3',
+      file: Buffer.alloc(0)
+    };
+
+    describe('Fails', () => {
+      it('should fail uploading a podcast part without jwt. HTTP 401', () => {
+        return server
+          .inject({
+            method: 'POST',
+            url: '/v1/podcast-parts'
+          })
+          .then((response) => {
+            expect(response).to.be.a('object');
+            expect(response).to.include({ statusCode: 401 });
+            expect(response.result).to.deep.include({
+              statusCode: 401,
+              error: 'Unauthorized',
+              message: 'Missing authentication'
+            });
+          });
+      });
+
+      it('should fail if request content-type is not multipart. HTTP 415', () => {
+        return server
+          .inject({
+            method: 'POST',
+            url: '/v1/podcast-parts',
+            headers: {
+              contentType: 'application/json',
+              authorization: accessToken
+            }
+          })
+          .then((response) => {
+            expect(response).to.be.a('object');
+            expect(response).to.include({ statusCode: 415 });
+            expect(response.result).to.deep.include({
+              statusCode: 415,
+              error: 'Unsupported Media Type',
+              message: 'Unsupported Media Type'
+            });
+          });
+      });
+
+      it('should fail if podcast part file is not a stream. HTTP 422', async () => {
+        const payloadFormData = objectToFormData({
+          ...podcastPartPayload,
+          // Returns string and not ReadableStream
+          file: fs.readFileSync(
+            path.join(path.resolve('./'), 'test', 'files', 'blank.mp3')
+          )
+        });
+        const payloadStream = await getStream(payloadFormData);
+
+        return server
+          .inject({
+            method: 'POST',
+            url: '/v1/podcast-parts',
+            payload: payloadStream,
+            headers: {
+              ...payloadFormData.getHeaders(),
+              authorization: accessToken
+            }
+          })
+          .then((response) => {
+            expect(response).to.be.a('object');
+            expect(response).to.include({ statusCode: 422 });
+            expect(response?.result).to.include({
+              statusCode: 422,
+              error: 'Unprocessable Entity',
+              message: 'File is not a stream'
+            });
+          });
+      });
+
+      describe('Requirements', () => {
+        it('should fail if missing podcastName', async () => {
+          const payloadFormData = objectToFormData({
+            ...podcastPartPayload,
+            podcastName: null
+          });
+          const payloadStream = await getStream(payloadFormData);
+
+          return server
+            .inject({
+              method: 'POST',
+              url: '/v1/podcast-parts',
+              payload: payloadStream,
+              headers: {
+                ...payloadFormData.getHeaders(),
+                authorization: accessToken
+              }
+            })
+            .then((response) => {
+              expect(response).to.be.a('object');
+              expect(response).to.include({ statusCode: 400 });
+              expect(response.result).to.deep.include({
+                statusCode: 400,
+                error: 'Bad Request',
+                message: '"podcastName" is required'
+              });
+            });
+        });
+
+        it('should fail if missing filename', async () => {
+          const payloadFormData = objectToFormData({
+            ...podcastPartPayload,
+            filename: null
+          });
+          const payloadStream = await getStream(payloadFormData);
+
+          return server
+            .inject({
+              method: 'POST',
+              url: '/v1/podcast-parts',
+              payload: payloadStream,
+              headers: {
+                ...payloadFormData.getHeaders(),
+                authorization: accessToken
+              }
+            })
+            .then((response) => {
+              expect(response).to.be.a('object');
+              expect(response).to.include({ statusCode: 400 });
+              expect(response.result).to.deep.include({
+                statusCode: 400,
+                error: 'Bad Request',
+                message: '"filename" is required'
+              });
+            });
+        });
+
+        it('should fail if missing file', async () => {
+          const payloadFormData = objectToFormData({
+            ...podcastPartPayload,
+            file: null
+          });
+          const payloadStream = await getStream(payloadFormData);
+
+          return server
+            .inject({
+              method: 'POST',
+              url: '/v1/podcast-parts',
+              payload: payloadStream,
+              headers: {
+                ...payloadFormData.getHeaders(),
+                authorization: accessToken
+              }
+            })
+            .then((response) => {
+              expect(response).to.be.a('object');
+              expect(response).to.include({ statusCode: 400 });
+              expect(response.result).to.deep.include({
+                statusCode: 400,
+                error: 'Bad Request',
+                message: '"file" is required'
+              });
+            });
+        });
+
+        it('should fail if storageType is defined but with unknow storage. HTTP 422', async () => {
+          const payloadFormData = objectToFormData({
+            ...podcastPartPayload,
+            storageStrategy: 'unkown-storage'
+          });
+          const payloadStream = await getStream(payloadFormData);
+
+          return server
+            .inject({
+              method: 'POST',
+              url: '/v1/podcast-parts',
+              payload: payloadStream,
+              headers: {
+                ...payloadFormData.getHeaders(),
+                authorization: accessToken
+              }
+            })
+            .then((response) => {
+              expect(response).to.be.a('object');
+              expect(response).to.include({ statusCode: 422 });
+              expect(response.result).to.deep.include({
+                statusCode: 422,
+                error: 'Unprocessable Entity',
+                message: "At least one storage type doesn't exist"
+              });
+            });
+        });
+      });
+    });
+
+    describe('Success', () => {
+      it('should succeed uploading podcast part on local', async () => {
+        const payloadFormData = objectToFormData({
+          ...podcastPartPayload,
+          file: fs.createReadStream(
+            path.join(path.resolve('./'), 'test', 'files', 'blank.mp3')
+          )
+        });
+        const payloadStream = await getStream(payloadFormData);
+
+        return server
+          .inject({
+            method: 'POST',
+            url: '/v1/podcast-parts',
+            payload: payloadStream,
+            headers: {
+              ...payloadFormData.getHeaders(),
+              authorization: accessToken
+            }
+          })
+          .then((response) => {
+            expect(response).to.be.a('object');
+            expect(response).to.include({ statusCode: 200 });
+            expect(response?.result).to.include({
+              statusCode: 200
+            });
+            expect(response?.result?.data).to.have.property('filename');
+            expect(response?.result?.data).to.include({
+              location: 'podcasts/e-2-e-podcast',
+              storageType: 'local',
+              publicLink: undefined
+            });
+          });
+      });
+
+      it('should succeed uploading podcast part on aws', async () => {
+        const payloadFormData = objectToFormData({
+          ...podcastPartPayload,
+          file: fs.createReadStream(
+            path.join(path.resolve('./'), 'test', 'files', 'blank.mp3')
+          ),
+          storageStrategy: 'aws'
+        });
+        const payloadStream = await getStream(payloadFormData);
+
+        return server
+          .inject({
+            method: 'POST',
+            url: '/v1/podcast-parts',
+            payload: payloadStream,
+            headers: {
+              ...payloadFormData.getHeaders(),
+              authorization: accessToken
+            }
+          })
+          .then((response) => {
+            expect(response).to.be.a('object');
+            expect(response).to.include({ statusCode: 200 });
+            expect(response?.result).to.include({
+              statusCode: 200
+            });
+            expect(response?.result?.data).to.have.property('filename');
+            expect(response?.result?.data).to.include({
+              location: 'podcasts/e-2-e-podcast',
+              storageType: 'aws',
+              publicLink: `https://cosy-crafter-backup.s3.eu-west-3.amazonaws.com/podcasts/e-2-e-podcast/${response?.result?.data?.filename}`
+            });
+          });
+      });
+
+      it('should succeed uploading podcast part on scaleway', async () => {
+        const payloadFormData = objectToFormData({
+          ...podcastPartPayload,
+          file: fs.createReadStream(
+            path.join(path.resolve('./'), 'test', 'files', 'blank.mp3')
+          ),
+          storageStrategy: 'scaleway'
+        });
+        const payloadStream = await getStream(payloadFormData);
+
+        return server
+          .inject({
+            method: 'POST',
+            url: '/v1/podcast-parts',
+            payload: payloadStream,
+            headers: {
+              ...payloadFormData.getHeaders(),
+              authorization: accessToken
+            }
+          })
+          .then((response) => {
+            expect(response).to.be.a('object');
+            expect(response).to.include({ statusCode: 200 });
+            expect(response?.result).to.include({
+              statusCode: 200
+            });
+            expect(response?.result?.data).to.have.property('filename');
+            expect(response?.result?.data).to.include({
+              location: 'podcasts/e-2-e-podcast',
+              storageType: 'scaleway',
+              publicLink: `https://cosy-crafter.s3.fr-par.scw.cloud/podcasts/e-2-e-podcast/${response?.result?.data?.filename}`
+            });
+          });
+      });
+    });
+  });
+});
