c032cde826fa23064b7e2989beb0e7a892ed6336
lambertkevin <kvin.lambert@gmail.com>
2021-02-25 23:18:00 +0100

    Add  pool api e2e tests

---
+++ services/pool-service/test/e2e/poolApi.test.js
@@ -0,0 +1,194 @@
+import { expect } from 'chai';
+import ioClient from 'socket.io-client';
+import { startAuthService } from '../utils/authUtils';
+import init from '../../src/server';
+
+describe('Pool Api tests', () => {
+  let server;
+  let pool;
+  let pid;
+
+  before(async () => {
+    const authServiceChild = await startAuthService();
+    pid = authServiceChild.pid;
+    server = await init();
+    pool = ioClient.connect(
+      `http://${process.env.POOL_SERVICE_NAME}:${process.env.POOL_SERVICE_PORT}`
+    );
+  });
+
+  after(() => {
+    process.kill(pid);
+    server.close();
+  });
+
+  describe('Server Testing', () => {
+    it('should successfully connect to pool WS', (done) => {
+      pool.on('connect', () => {
+        expect(pool.connected).to.be.equal(true);
+        done();
+      });
+    });
+  });
+
+  describe('Job Addition', () => {
+    const filePodcast = {
+      id: '1234a1234b1234c1234d1234',
+      type: 'podcast-part',
+      seek: {
+        start: 0,
+        end: 10
+      }
+    };
+    const userInput = {
+      id: '1234e1234f1234g1234h1234',
+      type: 'user-input',
+      seek: {
+        start: 0,
+        end: 10
+      }
+    };
+    const jobPayload = {
+      name: 'e2e-job',
+      files: [filePodcast, userInput]
+    };
+
+    describe('Fails', () => {
+      describe('Requirements', () => {
+        it('should fail without name. ERROR CODE 400', (done) => {
+          pool.emit(
+            '/v1/jobs/add',
+            { ...jobPayload, name: null },
+            (response) => {
+              expect(response).to.deep.include({
+                statusCode: 400,
+                error: 'Bad Request',
+                message: '"name" must be a string'
+              });
+              done();
+            }
+          );
+        });
+
+        it('should fail without files. ERROR CODE 400', (done) => {
+          pool.emit(
+            '/v1/jobs/add',
+            { ...jobPayload, files: null },
+            (response) => {
+              expect(response).to.deep.include({
+                statusCode: 400,
+                error: 'Bad Request',
+                message: '"files" must be an array'
+              });
+              done();
+            }
+          );
+        });
+
+        it("should fail if file don't have id. ERROR CODE 400", (done) => {
+          const payload = {
+            ...jobPayload,
+            files: [{ ...filePodcast, id: null }]
+          };
+
+          pool.emit('/v1/jobs/add', payload, (response) => {
+            expect(response).to.deep.include({
+              statusCode: 400,
+              error: 'Bad Request',
+              message: '"files[0].id" must be a string'
+            });
+            done();
+          });
+        });
+
+        it('should fail if file id is not 24 characters long. ERROR CODE 400', (done) => {
+          const payload = {
+            ...jobPayload,
+            files: [{ ...filePodcast, id: '1' }]
+          };
+
+          pool.emit('/v1/jobs/add', payload, (response) => {
+            expect(response).to.deep.include({
+              statusCode: 400,
+              error: 'Bad Request',
+              message: '"files[0].id" length must be 24 characters long'
+            });
+            done();
+          });
+        });
+
+        it('should fail if file type is not podcast-part or user-input. ERROR CODE 400', (done) => {
+          const payload = {
+            ...jobPayload,
+            files: [{ ...filePodcast, type: 'e2e-type' }]
+          };
+
+          pool.emit('/v1/jobs/add', payload, (response) => {
+            expect(response).to.deep.include({
+              statusCode: 400,
+              error: 'Bad Request',
+              message:
+                '"files[0].type" must be one of [podcast-part, user-input]'
+            });
+            done();
+          });
+        });
+
+        it('should fail if file seek is not an object. ERROR CODE 400', (done) => {
+          const payload = {
+            ...jobPayload,
+            files: [
+              {
+                ...filePodcast,
+                seek: false
+              }
+            ]
+          };
+
+          pool.emit('/v1/jobs/add', payload, (response) => {
+            expect(response).to.deep.include({
+              statusCode: 400,
+              error: 'Bad Request',
+              message: '"files[0].seek" must be of type object'
+            });
+            done();
+          });
+        });
+
+        it('should fail if file seek contains a property other than start and end. ERROR CODE 400', (done) => {
+          const payload = {
+            ...jobPayload,
+            files: [
+              {
+                ...filePodcast,
+                seek: {
+                  test: 'e2e'
+                }
+              }
+            ]
+          };
+
+          pool.emit('/v1/jobs/add', payload, (response) => {
+            expect(response).to.deep.include({
+              statusCode: 400,
+              error: 'Bad Request',
+              message: '"files[0].seek.test" is not allowed'
+            });
+            done();
+          });
+        });
+      });
+    });
+
+    describe('Success', () => {
+      it('should succeed adding a job', (done) => {
+        pool.emit('/v1/jobs/add', jobPayload, (response) => {
+          expect(response?.data).to.have.property('location');
+          expect(response?.data).to.have.property('storageType');
+          expect(response?.data).to.have.property('publicLink');
+          done();
+        });
+      });
+    });
+  });
+});
