186a59213a27415940d036c1a2246fea0c2a8279
lambertkevin <kvin.lambert@gmail.com>
2021-02-25 17:32:31 +0100

    Add podcastStorage api tests

--- services/storage-service/test/utils/mockS3.js
+++ services/storage-service/test/utils/mockS3.js
@@ -1,30 +1,34 @@
+import fs from 'fs';
 import path from 'path';
 import S3rver from 's3rver';
+import { Storage, StorageType } from '@tweedegolf/storage-abstraction';
+import { S3, Endpoint } from 'aws-sdk';
 
 // Start fake S3 servers for aws and scaleway
 export default async () => {
   const aws = await new S3rver({
     hostname: '0.0.0.0',
-    silent: false,
+    port: 4501,
+    silent: true,
     directory: path.join(path.resolve('./'), 'bucket', 'tests', 'aws'),
-    resetOnClose: true,
     configureBuckets: [
       {
-        name: 'test-aws-bucket'
+        name: process.env.AWS_BUCKET_NAME
       }
     ]
-  });
+  }).run();
+
   const scaleway = await new S3rver({
     hostname: '0.0.0.0',
-    silent: false,
+    port: 4500,
+    silent: true,
     directory: path.join(path.resolve('./'), 'bucket', 'tests', 'scaleway'),
-    resetOnClose: true,
     configureBuckets: [
       {
-        name: 'test-scaleway-bucket'
+        name: process.env.SCALEWAY_BUCKET_NAME
       }
     ]
-  });
+  }).run();
 
   return [aws, scaleway];
 };
