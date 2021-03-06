4c4c7efb3407c11cd64fda1f8281ee00e5ff4d0d
lambertkevin <kvin.lambert@gmail.com>
2021-03-07 00:25:53 +0100

    Globalize NODE_ENV in job controller tests

--- services/pool-service/test/unit/jobController.test.js
+++ services/pool-service/test/unit/jobController.test.js
@@ -7,10 +7,17 @@ import {
 import { makeWorkerMock } from '../utils/workerUtils';
 
 describe('Job Controller Unit Tests', () => {
+  before(() => {
+    process.env.NODE_ENV = 'development';
+  });
+
+  after(() => {
+    process.env.NODE_ENV = 'test';
+  });
+
   describe('CreateTranscodingJob', () => {
     describe('Fails', () => {
       it('should fail to create and execute a transcoding job if payload is not decryptable', () => {
-        process.env.NODE_ENV = 'development';
         let response;
         const log = {};
         // Let's imagine the job or worker has the wrong set of key
@@ -25,30 +32,24 @@ describe('Job Controller Unit Tests', () => {
           }
         );
 
-        return transcodingJob
-          .start(worker)
-          .catch(() => {
-            expect(log).to.not.deep.include({
-              decryptedPayload: {
-                name: 'test',
-                files: [{ id: '123' }, { id: '456' }]
-              }
-            });
-            expect(response).to.deep.include({
-              statusCode: 500,
-              message: 'Transcode/Join failed'
-            });
-            // Job should have failed and been put back in queue
-            expect(transcodingJob.fails).to.be.equal(1);
-            expect(transcodingJob.status).to.be.equal(JOB_STATUS_WAITING);
-          })
-          .finally(() => {
-            process.env.NODE_ENV = 'test';
+        return transcodingJob.start(worker).catch(() => {
+          expect(log).to.not.deep.include({
+            decryptedPayload: {
+              name: 'test',
+              files: [{ id: '123' }, { id: '456' }]
+            }
           });
+          expect(response).to.deep.include({
+            statusCode: 500,
+            message: 'Transcode/Join failed'
+          });
+          // Job should have failed and been put back in queue
+          expect(transcodingJob.fails).to.be.equal(1);
+          expect(transcodingJob.status).to.be.equal(JOB_STATUS_WAITING);
+        });
       });
 
       it('should fail to create and execute a failing transcoding job', () => {
-        process.env.NODE_ENV = 'development';
         let response;
         const log = {};
         // Let's imagine the job or worker has the wrong set of key
@@ -63,30 +64,24 @@ describe('Job Controller Unit Tests', () => {
           }
         );
 
-        return transcodingJob
-          .start(worker)
-          .catch(() => {
-            expect(log?.decryptedPayload).to.deep.include({
-              name: 'test',
-              files: [{ id: '123' }, { id: '456' }]
-            });
-            expect(response).to.deep.include({
-              statusCode: 500,
-              message: 'Transcode/Join failed'
-            });
-            // Job should have failed and been put back in queue
-            expect(transcodingJob.fails).to.be.equal(1);
-            expect(transcodingJob.status).to.be.equal(JOB_STATUS_WAITING);
-          })
-          .finally(() => {
-            process.env.NODE_ENV = 'test';
+        return transcodingJob.start(worker).catch(() => {
+          expect(log?.decryptedPayload).to.deep.include({
+            name: 'test',
+            files: [{ id: '123' }, { id: '456' }]
           });
+          expect(response).to.deep.include({
+            statusCode: 500,
+            message: 'Transcode/Join failed'
+          });
+          // Job should have failed and been put back in queue
+          expect(transcodingJob.fails).to.be.equal(1);
+          expect(transcodingJob.status).to.be.equal(JOB_STATUS_WAITING);
+        });
       });
     });
 
     describe('Success', () => {
       it('should succeed to create and execute a transcoding job', () => {
-        process.env.NODE_ENV = 'development';
         let response;
         const log = {};
         const worker = makeWorkerMock(log);
@@ -101,7 +96,6 @@ describe('Job Controller Unit Tests', () => {
         );
 
         return transcodingJob.start(worker).then(() => {
-          process.env.NODE_ENV = 'test';
           expect(log?.decryptedPayload).to.deep.include({
             name: 'test',
             files: [{ id: '123' }, { id: '456' }]
