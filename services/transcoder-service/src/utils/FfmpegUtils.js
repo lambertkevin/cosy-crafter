6e7c4dc79f7f03d981cfff230e6f6bf1a7deb616
KevinMolotov <klambert@molotov.tv>
2021-03-07 22:10:48 +0100

    Add ffmpeg utils unit test + refactoring of joinFiles in transcodeController

--- services/transcoder-service/src/utils/FfmpegUtils.js
+++ services/transcoder-service/src/utils/FfmpegUtils.js
@@ -7,14 +7,20 @@
  * @return {Array<String>}
  */
 export const getCrossFadeFilters = (files) => {
-  const inputs = files.map((x) => x.input);
-  const inputsMixed = inputs.reduce((acc, curr, i) => {
+  if (!files || !Array.isArray(files)) {
+    return null;
+  }
+
+  // Making an array of what chaining inputs will look like
+  // E.g.: input A crossfaded with input B will create output AB
+  // So for 4 inputs, we should get ['0', '01', '012', '0123']
+  const inputsMixed = files.reduce((acc, input, i) => {
     if (i === 0) {
-      acc.push(curr);
+      acc.push(`${i}`);
       return acc;
     }
 
-    acc.push(acc[acc.length - 1] + curr);
+    acc.push(`${acc[acc.length - 1]}${i}`);
     return acc;
   }, []);
 
@@ -25,21 +31,56 @@ export const getCrossFadeFilters = (files) => {
             filter: 'acrossfade',
             options: {
               d: 4,
+              /**  @WARNING Change crossfade depending on input-type podcast-part or user-input */
               c1: 'log',
               c2: 'nofade'
             },
-            inputs: [inputsMixed[i], inputs[i + 1]],
+            // Take as input the last output done
+            // E.g. We are at input 1 so last output has been
+            // the mixing of '0' and '1' merging in output '01' and
+            // we are now merging this '01' with '2' into output '012'
+            inputs: [inputsMixed[i], `${i + 1}`],
             outputs: [inputsMixed[i + 1]]
           }
         : null;
     })
     .filter((x) => x);
 
+  // The last 'output' is implied by the fact
+  // it's the last merging/crossfade and
+  // therefore we need to remove it
   if (crossFadeFilters.length) {
     delete crossFadeFilters[crossFadeFilters.length - 1].outputs;
   }
 
   return crossFadeFilters.length ? crossFadeFilters : null;
+};
+
+/**
+ * Returns a percentage from a timemark format
+ * based on the total duration
+ *
+ * @param {String} timemark [format HH:mm:ss.SSS]
+ * @param {Number} duration
+ *
+ * @return {Number} percentage
+ */
+export const percentageFromTimemark = (timemark, duration) => {
+  if (typeof timemark !== 'string' || typeof duration !== 'number') {
+    return 0;
+  }
+
+  const timemarkRegex = new RegExp('[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{3}');
+  if (!timemarkRegex.test(timemark)) {
+    return 0;
+  }
+
+  const timemarkAsSec =
+    (Date.parse(`04/21/2014 ${timemark}`) -
+      Date.parse('04/21/2014 00:00:00.00')) /
+    1000;
+
+  return (timemarkAsSec / duration) * 100;
 };
 
 export default {
---
+++ services/transcoder-service/test/unit/ffmpegUtils.test.js
@@ -0,0 +1,124 @@
+import path from 'path';
+import { expect } from 'chai';
+import {
+  getCrossFadeFilters,
+  percentageFromTimemark
+} from '../../src/utils/FfmpegUtils';
+
+describe('FFmpeg Utils Unit Tests', () => {
+  describe('getCrossFadeFilters', () => {
+    describe('Success', () => {
+      it('should create an array crossfade complex filters for each input', () => {
+        const files = Array(8).fill({
+          path: path.resolve('./test/files/10-seconds-of-silence.mp3')
+        });
+        const filters = getCrossFadeFilters(files);
+        expect(filters).to.include.deep.ordered.members([
+          {
+            filter: 'acrossfade',
+            options: { d: 4, c1: 'log', c2: 'nofade' },
+            inputs: ['0', '1'],
+            outputs: ['01']
+          },
+          {
+            filter: 'acrossfade',
+            options: { d: 4, c1: 'log', c2: 'nofade' },
+            inputs: ['01', '2'],
+            outputs: ['012']
+          },
+          {
+            filter: 'acrossfade',
+            options: { d: 4, c1: 'log', c2: 'nofade' },
+            inputs: ['012', '3'],
+            outputs: ['0123']
+          },
+          {
+            filter: 'acrossfade',
+            options: { d: 4, c1: 'log', c2: 'nofade' },
+            inputs: ['0123', '4'],
+            outputs: ['01234']
+          },
+          {
+            filter: 'acrossfade',
+            options: { d: 4, c1: 'log', c2: 'nofade' },
+            inputs: ['01234', '5'],
+            outputs: ['012345']
+          },
+          {
+            filter: 'acrossfade',
+            options: { d: 4, c1: 'log', c2: 'nofade' },
+            inputs: ['012345', '6'],
+            outputs: ['0123456']
+          },
+          {
+            filter: 'acrossfade',
+            options: { d: 4, c1: 'log', c2: 'nofade' },
+            inputs: ['0123456', '7']
+          }
+        ]);
+      });
+
+      it('should fail or throw if array of files is empty', () => {
+        const files = [];
+        const filters = getCrossFadeFilters(files);
+        expect(filters).to.be.equal(null);
+      });
+
+      it('should fail or throw if array of files is null', () => {
+        const filters = getCrossFadeFilters(null);
+        expect(filters).to.be.equal(null);
+      });
+
+      it('should fail or throw if array of files is undefined', () => {
+        const filters = getCrossFadeFilters(undefined);
+        expect(filters).to.be.equal(null);
+      });
+    });
+  });
+
+  describe('percentageFromTimemark', () => {
+    describe('Fails', () => {
+      it('should return 0 if timemark is wrongly formatted', () => {
+        const percentage = percentageFromTimemark('00:30:00', 3600);
+        expect(percentage).to.be.equal(0);
+      });
+
+      it('should return 0 if duration is not a number', () => {
+        const percentage = percentageFromTimemark('00:30:00.000', '3600');
+        expect(percentage).to.be.equal(0);
+      });
+
+      it('should return 0 if timemark is null', () => {
+        const percentage = percentageFromTimemark(null, 3600);
+        expect(percentage).to.be.equal(0);
+      });
+
+      it('should return 0 if timemark is undefined', () => {
+        const percentage = percentageFromTimemark(undefined, 3600);
+        expect(percentage).to.be.equal(0);
+      });
+
+      it('should return 0 if duration is null', () => {
+        const percentage = percentageFromTimemark('00:30:00.000', null);
+        expect(percentage).to.be.equal(0);
+      });
+
+      it('should return 0 if duration is undefined', () => {
+        const percentage = percentageFromTimemark('00:30:00.000', undefined);
+        expect(percentage).to.be.equal(0);
+      });
+    });
+
+    describe('Success', () => {
+      it('should get percentage 50% for 30min on 1 hour', () => {
+        const percentage = percentageFromTimemark('00:30:00.000', 3600);
+        expect(percentage).to.be.equal(50);
+      });
+
+      it('should get percentage 75% for 45min on 1 hour', () => {
+        const percentage = percentageFromTimemark('00:45:00.000', 3600);
+        expect(percentage).to.be.equal(75);
+      });
+    });
+  });
+});
