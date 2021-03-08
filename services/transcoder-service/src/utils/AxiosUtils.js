a8d2e23e467ea38c0686f2d020b597c29eff32c1
KevinMolotov <klambert@molotov.tv>
2021-03-08 01:53:48 +0100

    Remove useless exportable axios util

--- services/transcoder-service/src/utils/AxiosUtils.js
+++ services/transcoder-service/src/utils/AxiosUtils.js
@@ -9,47 +9,6 @@ import { refresh, tokens } from '../auth';
  *
  * @return {Promise}
  */
-export const legacyMakeAxiosInstance = () => {
-  const axiosInstance = axios.create();
-
-  /**
-   * Before each request, we'll add a possible retry if the
-   * request need a refreshed token
-   */
-  axiosInstance.interceptors.request.use((request) => {
-    if (typeof _.get(request, ['retried']) === 'undefined') {
-      // eslint-disable-next-line no-param-reassign
-      request.retried = false;
-    }
-
-    return request;
-  });
-
-  /**
-   * After a request with an error, we'll check if the error is a token not refreshed.
-   * If we didn't already tried to refresh, we'll just fallback to the errorHandler function
-   */
-  axiosInstance.interceptors.response.use(null, async (error) => {
-    const statusCode = _.get(error, ['response', 'data', 'statusCode']);
-    const message = _.get(error, ['response', 'data', 'message']);
-    const { config: request = {} } = error;
-
-    if (statusCode === 401 && message === 'Expired token' && !request.retried) {
-      try {
-        await refresh();
-        request.retried = true;
-        _.set(request, ['headers', 'authorization'], tokens.accessToken);
-        return axiosInstance.request(request);
-      } catch (e) {
-        return error;
-      }
-    }
-    return error;
-  });
-
-  return axiosInstance;
-};
-
 export const makeAxiosInstance = () => {
   const axiosInstance = axios.create();
 
