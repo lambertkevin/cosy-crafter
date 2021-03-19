cd7bec43a3f7dee7ffae9dc6cfac383d2caf7fbe
lambertkevin <kvin.lambert@gmail.com>
2021-03-19 23:41:30 +0100

    Move scripts to its own folder

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set +e
-
-(cd ./services/auth-service && npm run test  -- --bail false)
-(cd ./services/podcast-service && npm run test  -- --bail false)
-(cd ./services/pool-service && npm run test-unit  -- --bail false)
-(cd ./services/pool-service && npm run test  -- --bail false)
-(cd ./services/storage-service && npm run test  -- --bail false)
-(cd ./services/transcoder-service && npm run test-unit  -- --bail false)
