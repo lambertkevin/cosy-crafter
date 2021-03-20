04590a116600c5257c3a7bb5c1abbae27753767b
KevinMolotov <klambert@molotov.tv>
2021-03-20 21:59:25 +0100

    Update test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,6 +2,7 @@
 set +e
 
 (cd ./shared/@cosy/array-to-projection && npm run test  -- --bail false)
+(cd ./shared/@cosy/axios-utils && npm run test  -- --bail false)
 (cd ./services/auth-service && npm run test  -- --bail false --timeout 30000)
 (cd ./services/podcast-service && npm run test  -- --bail false --timeout 30000)
 (cd ./services/pool-service && npm run test-unit  -- --bail false)
