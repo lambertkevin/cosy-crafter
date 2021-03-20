cb852bcfba99c5d5beb9679c891dd5b1b20c7830
KevinMolotov <klambert@molotov.tv>
2021-03-21 00:49:40 +0100

    add @cosy/hapi-fail-validation-handler to script

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,6 +3,7 @@ set +e
 
 (cd ./shared/@cosy/array-to-projection && npm run test  -- --bail false)
 (cd ./shared/@cosy/axios-utils && npm run test  -- --bail false)
+(cd ./shared/@cosy/hapi-fail-validation-handler && npm run test  -- --bail false)
 (cd ./services/auth-service && npm run test  -- --bail false --timeout 30000)
 (cd ./services/podcast-service && npm run test  -- --bail false --timeout 30000)
 (cd ./services/pool-service && npm run test-unit  -- --bail false)
