808a90e39380a5559cfbfafc41e7294bd0902f8d
lambertkevin <kvin.lambert@gmail.com>
2021-03-20 16:50:44 +0100

    Remove _id from service

--- services/auth-service/src/schemas/ServiceSchema.js
+++ services/auth-service/src/schemas/ServiceSchema.js
@@ -22,6 +22,9 @@ export const validationSchema = creationSchema.append({
 });
 
 export const responseSchema = validationSchema
+  .append({
+    _id: joi.string().length(24).required().example('5f3ec14cb2d104269d3c3282')
+  })
   .fork(hiddenProperties, (x) =>
     x.optional().description('Only if not sanitized')
   )
