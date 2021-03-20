2e37dded8ea5a55a02c5a56b2fe54bc41ea27e4c
lambertkevin <kvin.lambert@gmail.com>
2021-03-20 19:19:10 +0100

    Update test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set +e
 
+(cd ./shared/@cosy/array-to-projection && npm run test  -- --bail false)
 (cd ./services/auth-service && npm run test  -- --bail false --timeout 30000)
 (cd ./services/podcast-service && npm run test  -- --bail false --timeout 30000)
-(cd ./services/pool-service && npm run test-unit  -- --bail false --timeout 30000)
+(cd ./services/pool-service && npm run test-unit  -- --bail false)
 (cd ./services/pool-service && npm run test  -- --bail false --timeout 30000)
 (cd ./services/storage-service && npm run test  -- --bail false --timeout 30000)
-(cd ./services/transcoder-service && npm run test-unit  -- --bail false --timeout 30000)
+(cd ./services/transcoder-service && npm run test-unit  -- --bail false)
