a06eba8aaa715b887545451465cf6ce790fcb524
KevinMolotov <klambert@molotov.tv>
2021-03-21 18:56:18 +0100

    Istanbul ignore default param @cosy/custom-error

--- shared/@cosy/custom-error/index.js
+++ shared/@cosy/custom-error/index.js
@@ -1,4 +1,5 @@
 export default class CustomError extends Error {
+  // istanbul ignore next
   constructor(message, name = "CustomError", code, details) {
     super(message);
     this.name = name;
