168b3c90319ab211574cf046e8c57d57ea136288
KevinMolotov <klambert@molotov.tv>
2021-03-21 18:56:24 +0100

    Update test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,6 +4,8 @@ set +e
 (cd ./shared/@cosy/array-to-projection && npm run test  -- --bail false)
 (cd ./shared/@cosy/axios-utils && npm run test  -- --bail false)
 (cd ./shared/@cosy/hapi-fail-validation-handler && npm run test  -- --bail false)
+(cd ./shared/@cosy/json-api-standardize && npm run test  -- --bail false)
+(cd ./shared/@cosy/logger && npm run test  -- --bail false)
 (cd ./services/auth-service && npm run test  -- --bail false --timeout 30000)
 (cd ./services/podcast-service && npm run test  -- --bail false --timeout 30000)
 (cd ./services/pool-service && npm run test-unit  -- --bail false)
