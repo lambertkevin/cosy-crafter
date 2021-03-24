97ce384b9661295c02ba2c9e100d359d9bcc30e7
KevinMolotov <klambert@molotov.tv>
2021-03-24 14:49:30 +0100

    Change authUtils console.logs

--- services/pool-service/test/utils/authUtils.js
+++ services/pool-service/test/utils/authUtils.js
@@ -10,23 +10,22 @@ import { spawn } from 'child_process';
 export const startAuthService = () =>
   // Promise starting the auth-service locally
   new Promise((resolve, reject) => {
+    console.log('\x1b[2m🔑 Auth-Service starting...\x1b[0m');
     const child = spawn('npm', ['run', 'mock'], {
       cwd: path.resolve('./', '..', 'auth-service')
     });
+    const childPid = child.pid;
 
     child.on('exit', () => {
-      console.log('\x1b[43m🔑 Auth-Service killed\x1b[0m');
-      reject(
-        new Error('You must kill the process running on the auth-service port')
-      );
+      console.log(`\x1b[43m🔑 Auth-Service (#${childPid}) killed\x1b[0m`);
+      reject(new Error('You must kill the process running on the auth-service port'));
     });
 
     child.stdout.on('data', (data) => {
       // Service is ready when it logs 'Server running'
       if (data.toString().includes('Server running')) {
-        console.log(
-          `\x1b[42m🔑 Auth-Service spwaned:\x1b[0m ${data.toString().trim()}`
-        );
+        // prettier-ignore
+        console.log(`\x1b[42m🔑 Auth-Service (#${childPid}) spwaned:\x1b[0m ${data.toString().trim()}`);
         resolve(child);
       }
     });
