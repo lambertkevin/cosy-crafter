7cfc591261f9aa3da2464e02df0b40887ac1b7fe
lambertkevin <kvin.lambert@gmail.com>
2021-04-03 20:19:32 +0200

    Update scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 set +e
 
-(cd ./services/auth-service && rm -rf node_modules && npm i)
-(cd ./services/podcast-service && rm -rf node_modules && npm i)
-(cd ./services/pool-service && rm -rf node_modules && npm i)
-(cd ./services/storage-service && rm -rf node_modules && npm i)
-(cd ./services/transcoder-service && rm -rf node_modules && npm i)
+
+(cd ./shared/@cosy/array-to-projection  && rm -rf node_modules && npm i)
+(cd ./shared/@cosy/axios-utils  && rm -rf node_modules && npm i)
+(cd ./shared/@cosy/hapi-fail-validation-handler  && rm -rf node_modules && npm i)
+(cd ./shared/@cosy/json-api-standardize  && rm -rf node_modules && npm i)
+(cd ./shared/@cosy/logger  && rm -rf node_modules && npm i)
+(cd ./shared/@cosy/rsa-utils  && rm -rf node_modules && npm i)
+(cd ./shared/@cosy/schema-utils  && rm -rf node_modules && npm i)
+(cd ./shared/@cosy/auth  && rm -rf node_modules && npm i)
+(cd ./services/auth-service  && rm -rf node_modules && npm i)
+(cd ./services/podcast-service  && rm -rf node_modules && npm i)
+(cd ./services/pool-service  && rm -rf node_modules && npm i)
+(cd ./services/storage-service  && rm -rf node_modules && npm i)
+(cd ./services/transcoder-service  && rm -rf node_modules && npm i)
