0fd1859709d56d73bc39619c6a7f84d41bfb86f7
lambertkevin <kvin.lambert@gmail.com>
2021-04-04 19:04:15 +0200

    Add gateway-service doc + errorHandler

--- services/gateway-service/src/server.js
+++ services/gateway-service/src/server.js
@@ -1,38 +1,44 @@
 import os from 'os';
+import boom from '@hapi/boom';
 import express from 'express';
 import * as Sentry from '@sentry/node';
 import * as Tracing from '@sentry/tracing';
 import { createProxyMiddleware } from 'http-proxy-middleware';
+import { identifier } from './config';
+import pkg from '../package.json';
 
 export default () => {
   const app = express();
 
-  if (process.env.NODE_ENV === 'production') {
-    Sentry.init({
-      dsn: process.env.SENTRY_DSN_GATEWAY_SERVICE,
-      integrations: [
-        // enable HTTP calls tracing
-        new Sentry.Integrations.Http({ tracing: true }),
-        // enable Express.js middleware tracing
-        new Tracing.Integrations.Express({ app })
-      ],
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN_GATEWAY_SERVICE,
+    integrations: [
+      // enable HTTP calls tracing
+      new Sentry.Integrations.Http({ tracing: true }),
+      // enable Express.js middleware tracing
+      new Tracing.Integrations.Express({ app })
+    ],
+    environment: process.env.NODE_ENV,
+    serverName: identifier,
+    attachStacktrace: true,
+    release: pkg.version,
+    normalizeDepth: 11,
+    tracesSampleRate: 1.0
+  });
 
-      // Set tracesSampleRate to 1.0 to capture 100%
-      // of transactions for performance monitoring.
-      // We recommend adjusting this value in production
-      tracesSampleRate: 1.0
-    });
+  // RequestHandler creates a separate execution context using domains, so that every
+  // transaction/span/breadcrumb is attached to its own Hub instance
+  app.use(Sentry.Handlers.requestHandler());
+  // TracingHandler creates a trace for every incoming request
+  app.use(Sentry.Handlers.tracingHandler());
+  // The error handler must be before any other error middleware and after all controllers
+  app.use(Sentry.Handlers.errorHandler());
 
-    // RequestHandler creates a separate execution context using domains, so that every
-    // transaction/span/breadcrumb is attached to its own Hub instance
-    app.use(Sentry.Handlers.requestHandler());
-    // TracingHandler creates a trace for every incoming request
-    app.use(Sentry.Handlers.tracingHandler());
-
-    // The error handler must be before any other error middleware and after all controllers
-    app.use(Sentry.Handlers.errorHandler());
-  }
-
+  /**
+   * Proxy for podcast-service
+   *
+   * @method HTTP
+   */
   app.use(
     '/podcast',
     createProxyMiddleware({
@@ -42,6 +48,11 @@ export default () => {
     })
   );
 
+  /**
+   * Proxy for auth-service
+   *
+   * @method HTTP
+   */
   app.use(
     '/auth',
     createProxyMiddleware({
@@ -51,6 +62,11 @@ export default () => {
     })
   );
 
+  /**
+   * Proxy for storage-service
+   *
+   * @method HTTP
+   */
   app.use(
     '/storage',
     createProxyMiddleware({
@@ -60,6 +76,11 @@ export default () => {
     })
   );
 
+  /**
+   * Proxy for pool-service
+   *
+   * @method HTTP
+   */
   app.use(
     '/pool',
     createProxyMiddleware({
@@ -69,19 +90,35 @@ export default () => {
     })
   );
 
-  const wsProxy = createProxyMiddleware('/socket.io', {
-    target: `ws://${process.env.POOL_SERVICE_NAME}:${process.env.POOL_SERVICE_PORT}`,
-    changeOrigin: true,
-    ws: true
+  /**
+   * Proxy for pool-service
+   *
+   * @method WebSocket
+   */
+  app.use(
+    '/socket.io',
+    createProxyMiddleware('/socket.io', {
+      target: `ws://${process.env.POOL_SERVICE_NAME}:${process.env.POOL_SERVICE_PORT}`,
+      changeOrigin: true,
+      ws: true
+    })
+  );
+
+  /**
+   * Middleware to respond a Boom 404 error
+   * for every path & method not under
+   * the scope of the proxies
+   *
+   * @method GET,POST,PATCH,DELETE
+   */
+  app.use((req, res, next) => {
+    const error = boom.notFound();
+    res.status(404).send(error?.output?.payload);
   });
 
-  app.use('/socket.io', wsProxy);
-
-  const server = app.listen(process.env.GATEWAY_SERVICE_PORT, () => {
+  app.listen(process.env.GATEWAY_SERVICE_PORT, () => {
     console.log(`Server running on http://${os.hostname()}:${process.env.GATEWAY_SERVICE_PORT}`);
   });
-
-  server.on('upgrade', wsProxy.upgrade); // <-- subscribe to http 'upgrade'
 
   return app;
 };
